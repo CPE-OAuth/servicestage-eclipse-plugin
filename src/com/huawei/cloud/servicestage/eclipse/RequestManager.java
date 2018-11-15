4badf2eb968e068fc4ccbbc08b692681b0bf5890
Farhan Arshad <farhan3@users.noreply.github.com>
2018-11-15 14:22:53 -0500

    Update RequestManager.java

--- src/com/huawei/cloud/servicestage/eclipse/RequestManager.java
+++ src/com/huawei/cloud/servicestage/eclipse/RequestManager.java
@@ -220,9 +220,13 @@ public class RequestManager {
 
         r.serviceId = store.getString(PreferenceConstants.SERVICE_ID);
 
+        r.parameters = new ServiceInstanceRequestBody.Parameters();
+
         LocalDateTime dt = LocalDateTime.now();
         String date = dt.format(formatter);
         r.parameters.version = ds.get(ConfigConstants.APP_VERSION) + "-" + date;
+
+        r.parameters.source = new ServiceInstanceRequestBody.Parameters.Source();
 
         r.parameters.source = new ServiceInstanceRequestBody.Parameters.Source();
         r.parameters.source.type = ds.get(ConfigConstants.SOURCE_TYPE_OPTION);
